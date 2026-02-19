59c911f2350860b3836b5dcf468e3c54a2e1753e
romantsakunov <romantsakunov@gmail.com>
2026-02-20 02:47:01 +0300

    Revert "Написал автотест на создание заказа"
    This reverts commit 3dc064a3e91b0b4dafef1e30dbeb85ac8999edb6.

--- src/test/java/com/learning/AppTest.java
+++ src/test/java/com/learning/AppTest.java
@@ -1,11 +1,8 @@
 package com.learning;
 
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Простой тест для метода add класса App.
@@ -17,37 +14,5 @@ class AppTest {
         assertEquals(5, App.add(2, 3));
         assertEquals(0, App.add(-1, 1));
         assertEquals(-5, App.add(-2, -3));
-    }
-
-    @Test
-    void createStoreOrderTest() {
-        RestAssured.baseURI = "https://petstore.swagger.rv-school.ru/api/v3";
-
-        String requestBody = """
-                {
-                  "id": 10,
-                  "petId": 198772,
-                  "quantity": 7,
-                  "shipDate": "2026-02-19T23:21:20.605Z",
-                  "status": "approved",
-                  "complete": true
-                }
-                """;
-
-        Response response = RestAssured
-                .given()
-                .header("Content-Type","application/json")
-                .body(requestBody)
-                .when()
-                .post("/store/order")
-                .then()
-                .extract().response();
-
-        assertEquals(200, response.statusCode(), "Неверный статус код");
-        assertEquals(10, response.jsonPath().getInt("id"), "Неверный id заказа");
-        assertEquals(198772, response.jsonPath().getInt("petId"));
-        assertEquals(7, response.jsonPath().getInt("quantity"));
-        assertEquals("approved", response.jsonPath().getString("status"));
-        assertTrue(response.jsonPath().getBoolean("complete"));
     }
 }
